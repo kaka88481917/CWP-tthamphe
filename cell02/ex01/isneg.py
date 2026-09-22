331f30463376b4209c377f6c11ae73ed0465a93b
kaka88481917 <kaka88481917@gmail.com>
2026-09-22 22:41:34 +0700

    done cell02

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+inp = int(input())
+if inp > 0:
+	print("This number is positive.")
+elif inp < 0:
+	print("This number is negative.")
+else:
+	print("This number is both positive and negative.")
---
+++ cell02/ex02/password.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+password = "p@ssw0rd"
+inp = input()
+if inp == password:
+	print("ACCESS GRANTED")
+else:
+	print("ACCESS DENIED")
+
