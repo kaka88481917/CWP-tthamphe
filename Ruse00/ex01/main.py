75083653fea3c405df8220045e86161cea473111
kakai_desu <109893219+kaka88481917@users.noreply.github.com>
2026-09-24 23:45:18 +0700

    2

--- Ruse00/ex01/main.py
+++ Ruse00/ex01/main.py
@@ -2,19 +2,13 @@ import sys
 from checkmate import checkmate
 
 def main():
-    # ตรวจสอบว่ามีการส่งไฟล์แนบมาทาง Arguments หรือไม่
-    if len(sys.argv) < 2:
-        print("Error")
-        return
-        
-    # วนลูปอ่านทุกๆ ไฟล์ที่ส่งเข้ามา
-    for file_path in sys.argv[1:]:
+    file = sys.argv[1:]
+    for path in file:
         try:
-            with open(file_path, 'r') as f:
-                board = f.read()
-                checkmate(board)
-        except Exception:
-            # หากไฟล์พัง หรืออ่านไม่ได้
+            with open(path, 'r') as f:
+                content = f.read()
+                checkmate(content)
+        except FileNotFoundError:
             print("Error")
 
 if __name__ == "__main__":
