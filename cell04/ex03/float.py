07d8b122f5e998fa90ac909081dc9c1a35521d96
kakai_desu <kaka88481917@gmail.com>
2026-09-24 10:58:52 +0700

    .

---
+++ cell04/ex03/float.py
@@ -0,0 +1,6 @@
+inp = input("Give me a number: ")
+
+if float(inp) % 1 == 0:
+    print("This number is an integer.")
+else:
+    print("This number is a decimal.")
---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,5 @@
+import math
+
+inp = float(input("Give me a number: "))
+
+print(math.ceil(inp))
