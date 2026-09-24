9c1febfaec1ae006fc8aac34112a081ff11b0510
kakai_desu <kaka88481917@gmail.com>
2026-09-24 13:44:40 +0700

    done till 5.3

--- cell05/ex03/play_with)arrays.py
+++ cell05/ex03/play_with)arrays.py
@@ -1,17 +1,9 @@
-before = [2, 8, 9, 48, 8, 22, -12, 2]
-after = []
-after_after = []
-after_after_after = set()
+arr = [2, 8, 9, 48, 8, 22, -12, 2]
+arr2 = [x+2 for x in arr]
+arr3 = [x for x in arr2 if x > 5]
+sett = set()
+for i in arr3:
+    sett.add(i)
 
-for num in before:
-    after.append(num + 2)
-
-for num in after:
-    if num > 5:
-        after_after.append(num)
-
-for num in after_after:
-    after_after_after.add(num)
-
-print(f"{before}")
-print(f"{after_after_after}")
+print(f'{arr}')
+print(f'{sett}')
